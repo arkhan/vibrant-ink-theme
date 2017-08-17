0653281ad1f640d91482220dfc7b33a66f0ce835
Edison Ibañez <e@ibaez.org>
2017-08-17 11:24:57 -0500

    carga inicial

---
+++ vibrant-theme.el
@@ -0,0 +1,46 @@
+;;; ujelly-theme.el --- Ujelly theme for GNU Emacs 24 (deftheme)
+
+;; Author: Mark Tran <mark.tran@gmail.com>
+;; URL: http://github.com/marktran/color-theme-ujelly
+;; Version: 1.2.9
+
+;; Inspired by jellybeans: http://www.vim.org/scripts/script.php?script_id=2555.
+(deftheme vibrant "Vibrant Ink color theme")
+
+(custom-theme-set-faces
+ 'vibrant-ink
+ '(default ((t (:background "#111111" :foreground "#ffffff"))))
+ '(cursor ((t (:background "#555577" :foreground "#ffffff"))))
+ '(region ((t (:background "#444444"))))
+ '(mode-line ((t (:background "#bfbfbf" :foreground "#000000"))))
+ '(mode-line-inactive ((t (:background "#e5e5e5" :foreground "#333333"))))
+ '(fringe ((t (:background "#000000"))))
+ '(minibuffer-prompt ((t (:foreground "#ff6600"))))
+ '(font-lock-builtin-face ((t (:foreground "#aaccff"))))
+ '(font-lock-comment-face ((t (:slant italic :foreground "#9933cc"))))
+ '(font-lock-constant-face ((t (:foreground "#339999"))))
+ '(font-lock-function-name-face ((t (:foreground "#ffcc00"))))
+ '(font-lock-keyword-face ((t (:foreground "#ff6600"))))
+ '(font-lock-string-face ((t (:foreground "#66ff00"))))
+ '(font-lock-type-face ((t (:foreground "#ffffff"))))
+ '(font-lock-variable-name-face ((t (:foreground "#ffffff"))))
+ '(font-lock-warning-face ((t (:foreground "#ffb5d0" :weight bold))))
+ '(erb-out-delim-face ((t (:foreground "#E6E1DC"))))
+ '(erb-delim-face ((t (:foreground "#E6E1DC" ))))
+ '(erb-exec-face ((t (:background "#191919"))))
+ '(erb-out-face ((t (:background "#191919"))))
+ '(isearch ((t (:background "#555555"))))
+ '(lazy-highlight ((t (:background "#444444"))))
+ '(link ((t (:foreground "#aaccff" :underline t))))
+ '(link-visited ((t (:foreground "#aaccff" :underline t))))
+ '(button ((t (:background "#bfbfbf" :underline t))))
+ '(header-line ((t (:background "#e5e5e5" :foreground "#333333")))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'vibrant-ink)
+
+;;; vibrant-theme.el ends here
