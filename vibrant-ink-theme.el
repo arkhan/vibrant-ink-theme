c4af2971782457f314adf89b21d0b69c796855c9
Edison Ibáñez <edison@disroot.org>
2019-07-30 16:49:17 -0500

    REVERT Backgroud

--- vibrant-ink-theme.el
+++ vibrant-ink-theme.el
@@ -11,7 +11,7 @@
 
 (let ((class '((class color) (min-colors 89)))
       (fg "#ffffff")
-      (bg "gray10")
+      (bg "#000000")
       (black-0 "#111111")
       (gray-0 "#444444")
       (gray-1 "#191919")
